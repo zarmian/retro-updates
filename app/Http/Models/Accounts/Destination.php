d50dbba1fc4507839d2473ff8c012e613a98b77b
zarmian <51716340+zarmian@users.noreply.github.com>
2020-03-20 04:02:48 +0500

    Create Destination.php

---
+++ app/Http/Models/Accounts/Destination.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Models\Accounts;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    public $table = 'tbl_destination';
+
+
+    public $timestamps = false;
+}
