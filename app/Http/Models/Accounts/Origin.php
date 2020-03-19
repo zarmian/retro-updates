6a650d2ff8c0ab18143596d64b56010e9fb06666
zarmian <51716340+zarmian@users.noreply.github.com>
2020-03-20 04:02:53 +0500

    Create Origin.php

---
+++ app/Http/Models/Accounts/Origin.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Models\Accounts;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Origin extends Model
+{
+    public $table = 'tbl_origin';
+
+
+    public $timestamps = false;
+}
