72200f057f01a6e4c2a4eeda211967f0207b5d5b
zarmian <51716340+zarmian@users.noreply.github.com>
2020-03-20 04:07:04 +0500

    Create 2020_03_17_212614_create_destination_table.php

---
+++ database/migrations/2020_03_17_212614_create_destination_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDestinationTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_destination', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('destination')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_destination');
+    }
+}
